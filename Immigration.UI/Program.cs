a4abc0fef3ee31cc2477efe1bb321c7937363dc6
lyankey <56731939+lyankey@users.noreply.github.com>
2020-09-28 14:00:01 -0400

    aligning

--- Immigration.UI/Program.cs
+++ Immigration.UI/Program.cs
@@ -10,6 +10,13 @@ namespace Immigration.UI
     {
         static void Main(string[] args)
         {
+            //we can put the data for the questions in here, an array of numbers
+            //we can put the 
+            //put a string array of all the text of the questions, that has to match the enum (an object) and the answer list - like "questionTextListString"
+
+            //and maybe a FlowArray |[ ] , [ ] , | - a multidimensional array - That tells them where they are on the flowchart. They will
+            //always come in at 0 on the FlowArray |[ 0, 1, 50] , [ 3, 4, 40 ] , [6 , 3, 33 ]
+
         }
     }
 }
